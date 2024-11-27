128137d868f3bcb8062919b87b07b4a53d773b1f
anonymous298 <tackletalha@gmail.com>
2024-11-27 19:12:13 +0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="my_package",          # Your package name
+    version="0.2",
+    author='Talha',
+    author_email='tackletalha@gmail.com',
+    description='This is a talha package',
+    long_description='This is a very very long description',
+    packages=find_packages(),
+    install_requires=[]
+)
---
+++ talhapackage/__init__.py
@@ -0,0 +1,12 @@
+class KuchToHai:
+    def __init__(self):
+        print('Kuch To Zaroor Hai')
+
+    def gandafunc(self, number):
+        print('Number nahi milega')
+
+    def achafunc(self, number):
+        print(f'Ye lo number {number}')
+
+    def __repr__(self) -> str:
+        return 'This class is just made to test my package '
